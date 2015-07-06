1a868c9bba17afb75b8f75d8c7228c6aeed2fbf0
Scott Christensen <mscottchristensen@gmail.com>
2015-07-06 11:00:11 -0600

    glew section compiles on windows

--- BenTutorial/windows/Errors.cpp
+++ BenTutorial/windows/Errors.cpp
@@ -1,8 +1,9 @@
 #include "Errors.h"
+#include <iostream>
+#include <SDL2/SDL.h>
 
 //Prints out an error message and exits the game
-void fatalError(std::string errorString) 
-{
+void fatalError(std::string errorString) {
     std::cout << errorString << std::endl;
     std::cout << "Enter any key to quit...";
     int tmp;
